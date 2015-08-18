6fee81b881ffa4c9530f507a7ca7393ea3d8b1a3
KingfishBattleCrew <ghostofrazgriz62@gmail.com>
2015-08-18 09:22:17 -0700

    Hello World
    This is the Hello World Programming Sectors

---
+++ Harrison Guo/Hello_World.java
@@ -0,0 +1,16 @@
+
+/**
+ * Hello World Programming Section A
+ * 
+ * Harrison Guo
+ * 8/17/2015, BlueJ 3.1.5 
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+   {
+   System.out.println("Hello World! From Harrison Guo");
+//Display the string
+   }
+    
+}
---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Hello World Programming Section B
+ * 
+ * @author Harrison Guo
+ * @version August 18, 2015
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World From Harrison Guo!");
+    }
+}
