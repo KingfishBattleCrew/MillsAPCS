653194dd12f74af836c6003e96ee4143bd275fef
KingfishBattleCrew <ghostofrazgriz62@gmail.com>
2015-08-21 08:32:04 -0700

    Pokemon

---
+++ Pokemon/Poke.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Poke here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Poke{
+    public static void main(String[] args){
+        System.out.println("Pikachu welcomes you to the world of Pokémon!");
+		System.out.println("\t (\\__/)");
+		System.out.println("\t (o^.^)");
+		System.out.println("\tz(_(\")(\")");
+    }
+}
+     
