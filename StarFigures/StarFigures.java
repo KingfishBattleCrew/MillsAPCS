abdbd38a354b28595318035232a8ec3e0d6621f2
KingfishBattleCrew <ghostofrazgriz62@gmail.com>
2015-08-25 08:52:27 -0700

    StarFigures
    It's a contest

---
+++ StarFigures/StarFigures.java
@@ -0,0 +1,21 @@
+public class StarFigures{
+    public static void combine(){
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+    }
+    public static void main(String[] args){
+        combine();
+        System.out.println();
+        combine();
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println();
+        System.out.println("  *");
+        System.out.println("  *");
+        System.out.println("  *");
+        combine();
+    }
+}
